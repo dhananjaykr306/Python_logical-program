ef46c6d26536a591745dd611a3d77b5bf44eeff6
Dhananjay kumar <107258319+dhananjaykr306@users.noreply.github.com>
2024-11-25 10:30:18 +0530

    [Dhananjay] Python program to find the average length of words in a sentence.

---
+++ Python_3.py
@@ -0,0 +1,33 @@
+"""
+3.For a given sentence, return the average word length.
+Note: Remember to remove punctuation first.
+sentence1 = "Hi all, my name is Tom...I am originally from Australia."
+"""
+'''
+    @Author: Dhananjay Kumar
+    @Date: 23-11-2024
+    @Last Modified by: Dhananjay Kumar
+    @Last Modified time: 23-11-2024
+    @Title: Python program to find the average length of words in a sentence.
+'''
+
+def average_word_length(sentence):
+    """
+    Description: This function takes a sentence as input and returns the average word length.
+    Input: A string representing a sentence.
+    Output: A float representing the average word length.
+    """
+    # Remove punctuation
+    sentence = sentence.replace(",", "").replace(".", "").replace("!", "").replace("?", "").replace(":", "").replace(";", "")
+    # Split sentence into words
+    words = sentence.split()
+    # Calculate average word length
+    average_length = sum(len(word) for word in words) / len(words)
+    return average_length
+
+def main():
+    sentence=input("Enter a sentence: ")
+    print("The average word length of the sentence is:", average_word_length(sentence))
+
+if __name__ == "__main__":
+    main()
